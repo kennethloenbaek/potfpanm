5499d23ff79f391e0addc7ed0b0db4f2bb69ba78
Kenneth Loenbaek <kenneth@loenbaek.net>
2020-02-10 22:33:51 +0100

    Added solutions class. Made polar plotter with click update for Cp

--- potfpanm/domain.py
+++ potfpanm/domain.py
@@ -122,7 +122,7 @@ class domain(object):
             ns += len(shape.pz)
         return out
 
-    def get_solution(self, Uinf, aoa_deg):
+    def _solution(self, Uinf, aoa_deg):
         sigma = np.linalg.solve(self.induction_matrix_to_solve().imag, -self._rhs(Uinf, aoa_deg).imag)
         sigma_out = np.empty_like(self.z)
         nz = 0
@@ -133,6 +133,19 @@ class domain(object):
             nz += len(shape.z)
             npz += len(shape.pz)
         return solution(shapes=self._shapes, sings=self._sings, sigma=sigma_out, Uinf=Uinf, aoa_deg=aoa_deg)
+
+    def polar(self, aoas_deg, Uinfs):
+        if isinstance(aoas_deg, (float, int)) and isinstance(Uinfs, (float, int)):
+            return self._solution(Uinf=Uinfs, aoa_deg=aoas_deg)
+        elif isinstance(Uinfs, (float, int)):
+            Uinfs = np.full_like(aoas_deg, Uinfs)
+        elif isinstance(aoas_deg, (float, int)):
+            aoas_deg = np.full_like(Uinfs, aoas_deg)
+
+        sols = np.empty_like(aoas_deg, dtype=object)
+        for i, (aoa, uinf) in enumerate(zip(aoas_deg, Uinfs)):
+            sols[i] = self._solution(Uinf=uinf, aoa_deg=aoa)
+        return solutions(sols)
 
     def _rhs(self, Uinf, aoa_deg):
         return Uinf * np.exp(1j * np.deg2rad(aoa_deg)) / self.tan
@@ -163,10 +176,46 @@ class solution(domain):
             pz = self.pz + self.nor * offset
             return self.u(pz)
 
+    def pressure(self, z, rho=1.225):
+        return 1/2*rho*self.u_free - 1/2*rho*np.abs(self.u(z))**2
+
     def Cp(self, u):
         return 1 - (np.abs(u) / np.abs(self.u_free)) ** 2
 
+    def forces(self, rho=1.225):
+        Fs = np.empty_like(self._shapes, dtype=np.complex)
+        for ishape, shape in enumerate(self._shapes):
+            Fs[ishape] = -np.sum(self.pressure(shape.pz, rho=rho) * shape.nor * shape.dp)
+        return Fs
+
+    def forcesld(self, rho=1.225):
+        return self.forces(rho=rho)*self.u_free.conj()/self._Uinf
+
+    @property
+    def Cforces(self):
+        rho = 1.0
+        forceslds = self.forcesld(rho=rho)
+        Cforces = np.empty_like(forceslds)
+        for i_shape, (forcesld, shape) in enumerate(zip(forceslds, self._shapes)):
+            Cforces[i_shape] = forcesld / (1 / 2 * rho * shape.scale * np.abs(self.u_free) ** 2)
+        return Cforces
+
+    def lift(self, rho=1.225):
+        return self.forcesld(rho=rho).imag
+
+    @property
+    def Cl(self):
+        return self.Cforces.imag
+
+    def drag(self, rho=1.225):
+        return self.forcesld(rho=rho).real
+
+    @property
+    def Cd(self):
+        return self.Cforces.real
+
     def u_grid(self, x=None, y=None, return_grid=True):
+        # TODO: Add cluster parameter
         if x is None or isinstance(x, (float, int)):
             x_min = self.x.min()
             x_max = self.x.max()
@@ -219,7 +268,9 @@ class solution(domain):
                 width=500, height=500, padding=0.1)
         if y == "Cp":
             u = self.u_surface(offset=offset, offset_stag=offset_stag)
-            return hv.Curve(np.array([self.px, self.Cp(u)]).T).opts(width=500, height=500, padding=0.1)
+            return hv.Curve(np.array([self.px, self.Cp(u)]).T, kdims=["x"], vdims="Cp").opts(width=500, height=500,
+                                                                                             padding=0.1,
+                                                                                             invert_yaxis=True)
 
     def plot_grid(self, x=None, y=None, vector_field=False):
         import holoviews as hv
@@ -231,3 +282,43 @@ class solution(domain):
             return qm * vf
         else:
             return qm
+
+
+class solutions(object):
+    def __init__(self, solutions = []):
+        self._solutions = deepcopy(solutions)
+
+    def __getitem__(self, item):
+        if isinstance(item, str):
+            for i_sol, sol in enumerate(self._solutions):
+                if i_sol == 0:
+                    val = getattr(sol, item)
+                    out = np.empty([len(self._solutions)]+list(val.shape), dtype=val.dtype)
+                    out[i_sol] = val.copy()
+                else:
+                    out[i_sol] = getattr(sol, item)
+            return out
+        if isinstance(item, int):
+            return self._solutions[item]
+
+
+    def plot_polar(self, iair=0):
+        import holoviews as hv
+        # Creating aoa vs. Cl curve
+        aoa = self["_aoa_deg"]
+        Cl = self["Cl"][:, iair]
+        curve = hv.Curve(np.array([aoa, Cl]).T, kdims="AoA", vdims="Cl")
+        points = hv.Points((aoa, Cl), kdims=["AoA", "Cl"]).opts(tools=['tap', 'hover'], size=6)
+        # Creating selection
+        selection = hv.streams.Selection1D(source=points)
+        # Callback function
+        def plot_Cp(index):
+            if not index:
+                index = list(range(len(aoa)))
+            out = dict()
+            for ind in index:
+                out[aoa[ind]] = self[ind].plot_surface()
+            return hv.NdOverlay(out, kdims='AoA').relabel('x vs Cp (AoA-legend)')
+        # Creating dynamic map
+        dmap = hv.DynamicMap(plot_Cp, streams=[selection])
+        return curve*points + dmap
