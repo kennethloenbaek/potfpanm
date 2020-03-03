71ad15eb843b0d9234ef3c31734ca28350398c9e
Kenneth Loenbaek <kenneth@loenbaek.net>
2020-03-03 21:15:56 +0100

    Started documentation for methods and classes

--- potfpanm/domain.py
+++ potfpanm/domain.py
@@ -2,15 +2,47 @@ import numpy as np
 from copy import deepcopy, copy
 
 class domain(object):
-    def __init__(self, shapes=[], sings=[]):
-        self._shapes = deepcopy(shapes)
-        self._sings = deepcopy(sings)
+    """
+    Domain class
+    Holds shapes in the domain.
+    .polar method can be used to run simulations for different angle of attack and flow speeds
+    Notice, when adding a shape a deepcopy is not made of the shape, so changing the original shape instance afterwards
+    will also affect the shape in the domain or another domain instance if multiple domains are created from the same
+    shape. To avoid this use the copy method on the shape.
+    Parameters
+    ----------
+    :param shapes None,list,shape: None will initilize empty domain, a list of shapes will add all shapes in the list to
+     the domain, a single shape can also be added.
+    :param singularities None,list,singularitie: None will initilize domain with out singularities, list will added the
+    list of singularities to the domain, a single singularitie can also be added
+    """
+    def __init__(self, shapes=None, singularities=None):
+        if shapes is None:  # If none is given, initialize empty domain
+            self._shapes = []
+        elif isinstance(shapes, list):  # If shape is list directly assign
+            self._shapes = shapes
+        else:  # if not list, convert to list
+            self._shapes = list(shapes)
 
-    def add_airfoil(self, airfoil):
-        self._shapes.append(deepcopy(airfoil))
+        if singularities is None:  # If none is given, initialize domain without singularities
+            self._singularities = []
+        elif isinstance(singularities, list):  # If singularities is list directly assign
+            self._singularities = shapes
+        else:  # if not list, convert to list
+            self._singularities = list(singularities)
+
+    def add_shape(self, shape):
+        """Adds a shape to the domain.
+        Notice, when adding a shape a deepcopy is not made of the shape, so changing the original shape instance
+        afterwards will also affect the shape in the domain or another domain instance if multiple domains are created
+        from the same shape. To avoid this use the copy method on the shape.
+        """
+        self._shapes.append(shape)
 
     @property
     def z(self):
+        """Shape-edges for all shapes (defalut concatenated arrays) as ndarray of complex numbers.
+        Use .with_sep() method to separate shapes by a value"""
         if hasattr(self, "_sep_val"):
             return self._insert_sep_val("z")
         else:
@@ -21,25 +53,39 @@ class domain(object):
             [np.concatenate([getattr(obj, name), np.array([self._sep_val])]) for obj in self._shapes])[:-1]
 
     def with_sep(self, val=np.nan + 1j * np.nan):
+        """Method to separate each shape by a value. It returns a copy of the instance where all values are separate by
+        val.
+        Examples:
+            >>> # To get all shape edges separate by 0
+            >>> z_with_sep_0 = domain_instance.with_sep(0j).z
+        """
         obj = copy(self)
         obj._sep_val = val
         return obj
 
     @property
     def x(self):
+        """x-component of shape-edges for all shapes (defalut concatenated arrays) as ndarray of floats.
+            Use .with_sep() method to separate shapes by a value"""
         return self.z.real
 
     @property
     def y(self):
+        """y-component of shape-edges for all shapes (defalut concatenated arrays) as ndarray of floats.
+            Use .with_sep() method to separate shapes by a value"""
         return self.z.imag
 
     @property
     def xy(self):
+        """Shape-edges for all shapes (defalut concatenated arrays) as 2D ndarray of floats.
+            Use .with_sep() method to separate shapes by a value"""
         z = self.z
         return np.array([z.real, z.imag]).T
 
     @property
     def pz(self):
+        """Shape-panel-center for all shapes (defalut concatenated arrays) as ndarray of complex numbers.
+                Use .with_sep() method to separate shapes by a value"""
         if hasattr(self, "_sep_val"):
             return self._insert_sep_val("pz")
         else:
@@ -47,14 +93,20 @@ class domain(object):
 
     @property
     def px(self):
+        """x-component of shape-panel-center for all shapes (defalut concatenated arrays) as ndarray of floats.
+                    Use .with_sep() method to separate shapes by a value"""
         return self.pz.real
 
     @property
     def py(self):
+        """y-component of shape-panel-center for all shapes (defalut concatenated arrays) as ndarray of floats.
+                            Use .with_sep() method to separate shapes by a value"""
         return self.pz.imag
 
     @property
     def dz(self):
+        """Panel-length for all shapes (defalut concatenated arrays) as ndarray of complex numbers.
+                Use .with_sep() method to separate shapes by a value"""
         if hasattr(self, "_sep_val"):
             return self._insert_sep_val("dz")
         else:
@@ -62,26 +114,49 @@ class domain(object):
 
     @property
     def dx(self):
+        """x-component of panel-length for all shapes (defalut concatenated arrays) as ndarray of floats.
+                            Use .with_sep() method to separate shapes by a value"""
         return self.dz.real
 
     @property
     def dy(self):
+        """y-component of panel-length for all shapes (defalut concatenated arrays) as ndarray of floats.
+                                    Use .with_sep() method to separate shapes by a value"""
         return self.dz.imag
 
     @property
     def dp(self):
+        """Panel-length for all shapes (defalut concatenated arrays) as ndarray of floats (dp=abs(dz)).
+                        Use .with_sep() method to separate shapes by a value"""
         return abs(self.dz)
 
     @property
     def tan(self):
+        """Tangent-complex-vector for all shapes (defalut concatenated arrays) as ndarray of complex numbers.
+                        Use .with_sep() method to separate shapes by a value"""
         return self.dz / self.dp
 
     @property
     def nor(self):
+        """Normal-complex-vector for all shapes (defalut concatenated arrays) as ndarray of complex numbers. (points out
+        of the shape)
+        Use .with_sep() method to separate shapes by a value"""
         return self.tan * 1j
 
-    def plot(self, ptype="shape"):
-        import holoviews as hv
+    def hvplot(self, ptype="shape"):
+        """
+        Plots the shapes in the domain (Notices, it requries holoviews)
+        Parameters
+        ----------
+        :param ptype str: Plot type. The following are excepted:
+        "shape": Plots the shape-edges with equal aspect-ratio
+        "vectors": Plots shape-edges, as well as panel-centers and tangential and normal vectors.
+        """
+        try:
+            import holoviews as hv
+        except ImportError:
+            raise ImportError("To use the plot method holoviews is requried, see more here: http://holoviews.org/")
+
         if ptype == "shape":
             # Shape of airfoil (edges)
             return hv.Curve(self.with_sep().xy).opts(aspect='equal')
@@ -92,7 +167,7 @@ class domain(object):
             vf.opts(rescale_lengths=False, magnitude=hv.dim('Magnitude') * 0.01, pivot="tail")
             vfnor = hv.VectorField(np.array([self.px, self.py, np.angle(self.nor), np.abs(self.nor)]).T, label="Normal")
             vfnor.opts(rescale_lengths=False, magnitude=hv.dim('Magnitude') * 0.01, pivot="tail", color="red")
-            return self.plot("shape") * pc * vf * vfnor
+            return self.hvplot("shape") * pc * vf * vfnor
 
     @staticmethod
     def pi2pj_matrix(pi, pj):
@@ -135,6 +210,16 @@ class domain(object):
         return solution(shapes=self._shapes, sings=self._sings, sigma=sigma_out, Uinf=Uinf, aoa_deg=aoa_deg)
 
     def polar(self, aoas_deg, Uinfs):
+        """
+        Solves the flow for a set of flow conditions (aoa and Uinf)
+
+        :param aoas_deg: Angle of attack (or angle of incidence) of the flow (in degrees)
+        :type aoas_deg: float,list,ndarray
+        :param Uinfs: Free stream flow velocity
+        :type Uinfs: float,list,ndarray
+        :return: solution or solutions instance (solution if only floats are given)
+        """
+
         if isinstance(aoas_deg, (float, int)) and isinstance(Uinfs, (float, int)):
             return self._solution(Uinf=Uinfs, aoa_deg=aoas_deg)
         elif isinstance(Uinfs, (float, int)):
@@ -152,23 +237,56 @@ class domain(object):
 
 
 class solution(domain):
+    """
+    solution class (child class of domain, but new shapes can not be added)
+    """
     def __init__(self, shapes, sings, sigma, Uinf, aoa_deg):
         super().__init__(shapes, sings)
         self._sigma = sigma
         self._Uinf = Uinf
         self._aoa_deg = aoa_deg
 
-    def add_airfoil(self, air):
+    @property
+    def aoa_deg(self):
+        """Angle of attack (or angle of incidence) of the flow in degrees"""
+        return self._aoa_deg
+
+    @property
+    def aoa_rad(self):
+        """Angle of attack (or angle of incidence) of the flow in radians"""
+        return np.deg2rad(self._aoa_deg)
+
+    def add_shape(self, shape):
+        """New shapes can not be added to solution instance"""
         raise ValueError("Airfoils can not be added to solution instance")
 
     def u(self, z):
-        return self.u_free + self.induction_matrix(z).dot(self._sigma)
+        """
+        Computes the flow velocity in the domain [m/s] (same unites as Uinf)
+        :param z: Points to compute the flow velocity at
+        :type z: 1D ndarray of complex numbers
+        :return: Flow velocity at z
+        :rtype: 1D ndarray of complex numbers
+        """
+        return self.u_inf + self.induction_matrix(z).dot(self._sigma)
 
     @property
-    def u_free(self):
-        return self._Uinf * np.exp(1j * np.deg2rad(self._aoa_deg))
+    def u_inf(self):
+        """Returns the free stream velocity as a complex number"""
+        return self._Uinf * np.exp(1j * self.aoa_rad)
 
     def u_surface(self, offset=None, offset_stag=None):
+        """
+        Computes the surface velocity for each of the shapes
+
+        :param offset: If given the surface velocity is computed at a surface that is offset by the value of offset
+        :type offset: float
+        :param offset_stag: If given the surface velocity is computed at a surface that increasing linearly from the
+        stagnation point. The value of offset_stag is the offset at the traling edge.
+        :type offset_stag: float
+        :return: Flow velocity at the surface (or with offset)
+        :rtype: 1D ndarray, dtype=complex
+        """
         if offset_stag is not None:
             pz = self.pz
             u_surf = self.u(pz)
@@ -182,10 +300,37 @@ class solution(domain):
             return self.u(pz)
 
     def pressure(self, z, rho=1.225):
-        return 1/2*rho*self.u_free - 1/2*rho*np.abs(self.u(z))**2
+        """
+        Computes the pressure at z [Pa]
+        :param z: Points to compute the pressure at
+        :type z: 1D ndarray, dtype=complex
+        :param rho: flow density
+        :type rho: float
+        :return: Pressure at z
+        :rtype: 1D ndarray, dtype=float
+        """
+        return 1 / 2 * rho * self.u_inf - 1 / 2 * rho * np.abs(self.u(z)) ** 2
 
-    def Cp(self, u):
-        return 1 - (np.abs(u) / np.abs(self.u_free)) ** 2
+    def Cp(self, z=None, u=None):
+        """
+        Computes the pressure coefficient at z [-]
+
+        :param z: Points to compute the pressure at (can be None in which case u needs to be given)
+        :type z: 1D ndarray, dtype=complex
+        :param u: Velocity to compute the pressure coefficient for (can be None in which case z needs to be given)
+        :type u: 1D ndarray, dtype=complex
+        :return: Pressure coefficient z
+        :rtype: 1D ndarray, dtype=float
+        """
+        if (z is not None) and (u is not None):
+            raise ValueError(f"Only one of z and u can be given to Cp method (z={z}, u={u})")
+        if (z is None) and (u is None):
+            raise ValueError(f"One of z and u need to be given for Cp method, but none was given (z={z}, u={u})")
+
+        if z is None:
+            return 1 - (np.abs(u) / np.abs(self.u_inf)) ** 2
+        else:
+            return 1 - (np.abs(self.u(z)) / np.abs(self.u_inf)) ** 2
 
     def forces(self, rho=1.225):
         Fs = np.empty_like(self._shapes, dtype=np.complex)
@@ -194,7 +339,7 @@ class solution(domain):
         return Fs
 
     def forcesld(self, rho=1.225):
-        return self.forces(rho=rho)*self.u_free.conj()/self._Uinf
+        return self.forces(rho=rho) * self.u_inf.conj() / self._Uinf
 
     @property
     def Cforces(self):
@@ -202,7 +347,7 @@ class solution(domain):
         forceslds = self.forcesld(rho=rho)
         Cforces = np.empty_like(forceslds)
         for i_shape, (forcesld, shape) in enumerate(zip(forceslds, self._shapes)):
-            Cforces[i_shape] = forcesld / (1 / 2 * rho * shape.scale * np.abs(self.u_free) ** 2)
+            Cforces[i_shape] = forcesld / (1 / 2 * rho * shape.scale * np.abs(self.u_inf) ** 2)
         return Cforces
 
     def lift(self, rho=1.225):
@@ -266,8 +411,12 @@ class solution(domain):
         else:
             return u
 
-    def plot_surface(self, x="x", y="Cp", offset=None, offset_stag=None):
-        import holoviews as hv
+    def hvplot_surface(self, x="x", y="Cp", offset=None, offset_stag=None):
+        try:
+            import holoviews as hv
+        except ImportError:
+            raise ImportError("To use the plot method holoviews is requried, see more here: http://holoviews.org/")
+
         if y == "u":
             return hv.Curve(np.array([self.px, np.abs(self.u_surface(offset=offset, offset_stag=offset_stag))]).T).opts(
                 width=500, height=500, padding=0.1)
@@ -275,13 +424,17 @@ class solution(domain):
             u = self.u_surface(offset=offset, offset_stag=offset_stag)
             return hv.Curve(np.array([self.px, self.Cp(u)]).T, kdims=["x"], vdims="Cp").opts(invert_yaxis=True)
 
-    def plot_grid(self, x=None, y=None, vector_field=False):
-        import holoviews as hv
+    def hvplot_grid(self, x=None, y=None, vector_field=False):
+        try:
+            import holoviews as hv
+        except ImportError:
+            raise ImportError("To use the plot method holoviews is requried, see more here: http://holoviews.org/")
+
         # Get velocity field
         xg, yg, u = self.u_grid(x=x, y=y)
         qm = hv.Image((xg[0, :], yg[:, 0], np.abs(u))).opts(aspect='equal')
         if vector_field is True:
-            vf = hv.VectorField((xg, yg, np.angle(u), np.abs(u)))
+            vf = hv.VectorField((xg, yg, np.    angle(u), np.abs(u)))
             return qm * vf
         elif vector_field is not False:
             step = vector_field
@@ -308,9 +461,28 @@ class solutions(object):
         if isinstance(item, int):
             return self._solutions[item]
 
+    @property
+    def aoa_deg(self):
+        return self["aoa_deg"]
 
-    def plot_polar(self, iair=0):
-        import holoviews as hv
+    @property
+    def aoa_rad(self):
+        return self["aoa_rad"]
+
+    @property
+    def Cl(self):
+        return self["Cl"]
+
+    @property
+    def Cd(self):
+        return self["Cd"]
+
+    def hvplot_polar(self, iair=0):
+        try:
+            import holoviews as hv
+        except ImportError:
+            raise ImportError("To use the plot method holoviews is requried, see more here: http://holoviews.org/")
+
         # Creating aoa vs. Cl curve
         aoa = self["_aoa_deg"]
         Cl = self["Cl"][:, iair]
--- potfpanm/shapes.py
+++ potfpanm/shapes.py
@@ -1,17 +1,33 @@
 import numpy as np
+from copy import deepcopy
 
 class airfoil(object):
+    """
+    Airfoil class
+    Creates a 2D panel mesh for an airfoil from a given set of x,y values.
+    It will make a transformation of the shape so it has LE at (0,0) and TE at (1,+-TE_thick), this might involve both
+    translation, rotation and scaling. These values can be adjusted after the class is created.
+    It will also insure that the points are order in clockwise direction.
+
+    Parameters
+    ----------
+    :param x ndarray,list: x-coordinate describing the airfoil shape. If no y is passed it is assumed
+    that x is a set of complex numbers of the shape.
+    :param y ndarray, list: y-coordinate describing the airfoil shape.
+
+    """
     def __init__(self, x, y=None):
         # Convert to complex number
         if y is None:
-            z = x
+            z = np.asfarray(x, dtype=complex)
         else:
-            z = x + 1j * y
+            z = np.asfarray(x) + 1j * np.asfarray(y)
 
         # Checking that coordinate direction is clock-wise (otherwise reverse z array)
         if z.imag.argmax() < z.imag.argmin(): z = z[::-1]
 
-        # Compute center, rotation and scale (x_min at (0,0), start and end point in the middel between x-axis, scale -> x_max-x_min=1)
+        # Compute center, rotation and scale (x_min at (0,0), start and end point in the middel between x-axis,
+        # scale -> x_max-x_min=1)
         # Center
         cen = z[z.real.argmin()]
         z -= cen
@@ -32,65 +48,86 @@ class airfoil(object):
 
     @property
     def z(self):
+        """Shape-edges as ndarray of complex numbers"""
         return self._z * self.scale * self._rot.conj() + self._center()
 
     @property
     def x(self):
+        """x-component of shape-edges as ndarray of floats"""
         return self.z.real
 
     @property
     def y(self):
+        """y-component of shape-edges as ndarray of floats"""
         return self.z.imag
 
     @property
     def xy(self):
+        """Shape-edges as 2D ndarray of floats"""
         z = self.z
         return np.array([z.real, z.imag]).T
 
     @property
     def pz(self):
+        """Shape-panel-center as ndarray of complex numbers"""
         z = self.z
         return (z[1:] + z[:-1]) / 2
 
     @property
     def px(self):
+        """x-component of shape-panel-center as ndarray of floats"""
         return self.pz.real
 
     @property
     def py(self):
+        """y-component of shape-panel-center as ndarray of floats"""
         return self.pz.imag
 
     @property
     def dz(self):
+        """Panel-length as ndarray of complex numbers"""
         return np.diff(self.z)
 
     @property
     def dx(self):
+        """x-component of panel-length as ndarray of floats"""
         return self.dz.real
 
     @property
     def dy(self):
+        """y-component of panel-length as ndarray of floats"""
         return self.dz.imag
 
     @property
     def dp(self):
+        """Panel-length as ndarray of floats (dp=abs(dz))"""
         return abs(self.dz)
 
     @property
     def tan(self):
+        """Tangent-complex-vector as ndarray of complex numbers"""
         return self.dz / self.dp
 
     @property
     def nor(self):
+        """Normal-vector as ndarray of complex numbers (points out of the shape)"""
         return self.tan * 1j
 
     @property
     def rot(self):
+        """rotation instance, the angle of the whole shape can be set by either angle_deg and angle_rad
+        (degrees and radians respecitivly)"""
         return self._rot
 
     @property
     def center(self):
+        """center instance, the shape can be translated in both x and y direction though x, y as a float or z as
+        complex number"""
         return self._center
+
+    @property
+    def copy(self):
+        return deepcopy(self)
 
     # Induction matrix methods
 
@@ -119,8 +156,20 @@ class airfoil(object):
         factor2 = 1 / 2 - sign * pi2pj / dpj
         return -(1j / (2 * np.pi) * ((factor1 * factor2) + sign)).conj()
 
-    def plot(self, ptype="shape"):
-        import holoviews as hv
+    def hvplot(self, ptype="shape"):
+        """
+        Plots the shape (Notices, it requries holoviews)
+        Parameters
+        ----------
+        :param ptype str: Plot type. The following are excepted:
+        "shape": Plots the shape-edges with equal aspect-ratio
+        "vectors": Plots shape-edges, as well as panel-centers and tangential and normal vectors.
+        """
+        try:
+            import holoviews as hv
+        except ImportError:
+            raise ImportError("To use the plot method holoviews is requried, see more here: http://holoviews.org/")
+
         if ptype == "shape":
             # Shape of airfoil (edges)
             return hv.Curve(self.xy).opts(aspect='equal', width=1000, height=500, padding=0.1)
@@ -142,6 +191,7 @@ class rotation(object):
 
     @property
     def angle_rad(self):
+        """Shape rotation angle in radians, this value can be changed"""
         return np.angle(self._rot)
 
     @angle_rad.setter
@@ -150,6 +200,7 @@ class rotation(object):
 
     @property
     def angle_deg(self):
+        """Shape rotation angle in degrees, this value can be changed"""
         return np.angle(self._rot, deg=True)
 
     @angle_deg.setter
@@ -157,9 +208,11 @@ class rotation(object):
         self._rot = np.exp(1j * np.deg2rad(val)).conj()
 
     def __call__(self):
+        """Returns complex rotation vector"""
         return self._rot
 
     def conj(self):
+        """Complex conjugate of rotation complex-vector"""
         return self._rot.conj()
 
     def __repr__(self):
@@ -173,6 +226,7 @@ class center(object):
 
     @property
     def x(self):
+        """x-component of shape translation, this value can be changed"""
         return self._cen.real
 
     @x.setter
@@ -181,13 +235,24 @@ class center(object):
 
     @property
     def y(self):
+        """y-component of shape translation, this value can be changed"""
         return self._cen.imag
 
     @y.setter
     def y(self, val):
         self._cen = self._cen.real + 1j * val
 
+    @property
+    def z(self):
+        """Shape translation as complex number, this value can be changed"""
+        return self._cen
+
+    @z.setter
+    def z(self, val):
+        self._cen = val
+
     def __call__(self):
+        """Returns the translations as complex number"""
         return self._cen
 
     def __repr__(self):
